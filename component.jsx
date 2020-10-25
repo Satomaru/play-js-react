6f3955f3de8a5ab4d9c4f1e84fd8dbe5e2d9deb2
STB a.k.a. Satomaru <Satomaru@users.noreply.github.com>
2020-10-25 09:02:43 +0900

    リファクタリング。

--- component.jsx
+++ component.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 
 export class Component extends React.Component {
 
+  alertWhenError = (callback, ...args) => {
+    try {
+      return callback(...args);
+    } catch (error) {
+      window.alert(error.message || error);
+      throw error;
+    }
+  }
+
+  handleClick = (event) => {
+    if (!this.props.disabled && this.props.onClick) {
+      this.alertWhenError(this.props.onClick, ...(this.createArgsOnClick(event)));
+    }
+  }
+
   render() {
     try {
       return this.createView();
